0dbbb7f8a4cf874d365ebe48d3cd291ac241af8c
R <2420446@cognizant.com>
2026-03-03 14:32:51 +0530

    Typing Skills assignment has been completed

---
+++ Typing Skills/script.js
@@ -0,0 +1,29 @@
+I am unable to log the hours in the timesheet for the week of July 1 to 4 because it is frozen.
+
+Please reject the week of July 1 to 4. I will resubmit that week
+
+
+
+Cookies --> JWT Token
+
+Session storage 20
+l
+
+
+12.40  session token epir 20 min
+
+token key encrpt 
+
+
+
+1  api call   20min< 21 min Token Expiry response 406 --> response login
+
+
+Session ID --> 1 --> login Id 101==> token ID--> JWT Token 
+
+Session ID--> 1--> login ID 102 --> token ID--> 
+
+
+User ID --> 101 --> Device ID --> 8973773
+
+userID --> 101 --> Force Login--> 
